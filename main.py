9ab7ae2b982d52405c6186ed58b7670a683822ed
NeonGamer3220 <neon.gamer322@gmail.com>
2026-07-25 19:15:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ INITIAL_EXTENSIONS = [
     "commands.weekly_report",
     "commands.regulator_panel",
     "commands.send_message",
+    "commands.queue_panel", # <--- EZT ADD HOZZÁ
 ]
 
 # Discord Bot Intents beállítása
